8515b26ea1d97c8cc9d5251c7478e6a6f2170d47
akkanop-x <akkanop.home@gmail.com>
2025-10-31 13:37:21 +0700

    [RED] Add failing test for testHelloBasic

---
+++ src/test/java/com/example/GreetingTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetingTest {
+    @Test
+    void testHelloBasic() {
+        assertEquals("Hello, World", "test failed");
+    }
+}
